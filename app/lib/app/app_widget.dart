761899a96c420b1567b9b81959c9fe560415475a
arthur <arthur@aiox.inf.br>
2022-02-16 14:36:37 -0300

    small corrections

--- app/lib/app/app_widget.dart
+++ app/lib/app/app_widget.dart
@@ -1,17 +1,15 @@
-import 'package:app/app/pages/register_page.dart';
-import 'package:app/app/shared/auth_repository.dart';
+import 'package:app/app/pages/start_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:asuka/asuka.dart' as asuka;
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RepositoryProvider(
-        create: (context) => AuthRepository(),
-        child: RegisterPage(),
+      theme: ThemeData(
+        primaryColor: Color(0xFFff6600),
       ),
+      home: StartPage(),
       builder: asuka.builder,
     );
   }
---
+++ app/lib/app/pages/start_page.dart
@@ -0,0 +1,44 @@
+import 'package:app/app/pages/register_page.dart';
+import 'package:app/app/shared/auth_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+                height: 300,
+                width: 300,
+                child: Image.asset("assets/images/logo.png")),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RepositoryProvider(
+                    create: (context) => AuthRepository(),
+                    child: RegisterPage(),
+                  ),
+                ),
+              );
+            },
+            child: Text('Cadastrar Produto'),
+          ),
+        ],
+      ),
+    );
+  }
+}
